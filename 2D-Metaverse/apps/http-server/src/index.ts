45f6d5a1f503c09aa175cc2220c2b34fac838cc9
MukeshAbhi <abhimukesh284@gmail.com>
2024-12-21 15:53:26 +0530

    added zod validation

--- 2D-Metaverse/apps/http-server/src/index.ts
+++ 2D-Metaverse/apps/http-server/src/index.ts
@@ -1,5 +1,8 @@
 import express from 'express';
 import { router } from './routes/v1';
+import client from '@repo/db/client';
+import {} from '@repo/my-types/types'
+
 
 export const app = express();
 
