3c008e58b40563f888f6f1e4f746258172e6dc2e
MukeshAbhi <abhimukesh284@gmail.com>
2025-02-22 12:31:58 +0530

    working

---
+++ Social/api/dist/index.js
@@ -0,0 +1,22 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const morgan_1 = __importDefault(require("morgan"));
+const routes_1 = require("./routes");
+require("dotenv/config");
+const db_1 = __importDefault(require("./db"));
+const app = (0, express_1.default)();
+const port = process.env.PORT || 3000;
+(0, db_1.default)();
+app.use((0, cors_1.default)());
+app.use(express_1.default.json({ limit: "10mb" }));
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use((0, morgan_1.default)("dev"));
+app.use("/api/v1", routes_1.router);
+app.listen(port, () => {
+    console.log(`Server running on ${port}`);
+});
---
+++ Social/api/src/index.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import { router } from "./routes";
+import 'dotenv/config'
+import dbConnection from "./db";
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+dbConnection();
+
+app.use(cors());
+app.use(express.json({ limit: "10mb"}));
+app.use(express.urlencoded({ extended: true }))
+
+app.use(morgan("dev"));
+
+app.use("/api/v1",router);
+
+app.listen(port,() => {
+    console.log(`Server running on ${port}` )
+});
